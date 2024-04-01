ae487dd41d22d07d0f309aa137e40c5bfe4644aa
Ricardo Hernández Osorio <2172040687@correo.ler.uam.mx>
2024-04-01 03:22:04 -0600

    C: countingsort -- Fill with zeros before

--- HackerRank/countingsort.c
+++ HackerRank/countingsort.c
@@ -4,6 +4,9 @@
 int* countingSort(int* arr, int n){
 	int maxVal = 100; // Supposing the max value is 100
 	int* freqs = (int*)malloc(sizeof(int)*maxVal);
+	for(int i=0; i<maxVal; i++){
+		freqs[i] = 0;
+	}
 	for(int i=0; i<n; i++){
 		freqs[arr[i]]++;
 	}
